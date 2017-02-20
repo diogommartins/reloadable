9badc0a38a5ce79a49477f3a541479e11a324622
diogommartins <magalhaesmartins@icloud.com>
2017-02-20 14:03:06 -0300

    Adicionando versão inicial do decorator reloadable

---
+++ tests/test_decorators.py
@@ -0,0 +1,53 @@
+from unittest import TestCase, mock
+from reloadable.decorators import reloadable, STOP_CONDITION_EXCEPTION
+
+
+class ReloadableDecoratorTests(TestCase):
+    def setUp(self):
+        self.exception_callback = mock.Mock()
+
+    def test_it_recovers_from_exception_until_KeyboardInterrupt(self):
+        func = mock.Mock(side_effect=[ValueError, STOP_CONDITION_EXCEPTION])
+        reloadable_func = reloadable()(func)
+
+        with self.assertRaises(STOP_CONDITION_EXCEPTION):
+            reloadable_func()
+
+        self.assertEqual(func.call_count, 2)
+
+    def test_it_recovers_from_multiple_exceptions_until_KeyboardInterrupt(self):
+        exceptions = [ValueError,
+                      TypeError,
+                      IndexError,
+                      KeyError,
+                      BrokenPipeError,
+                      AttributeError,
+                      Exception,
+                      STOP_CONDITION_EXCEPTION]
+        func = mock.Mock(side_effect=exceptions)
+        reloadable_func = reloadable()(func)
+
+        with self.assertRaises(STOP_CONDITION_EXCEPTION):
+            reloadable_func()
+
+        self.assertEqual(func.call_count, len(exceptions))
+
+    def test_it_calls_the_exception_callback(self):
+        exceptions = [ValueError,
+                      TypeError,
+                      IndexError,
+                      KeyError,
+                      BrokenPipeError,
+                      AttributeError,
+                      Exception,
+                      STOP_CONDITION_EXCEPTION]
+        func = mock.Mock(side_effect=exceptions)
+        mock_callback = mock.Mock()
+        reloadable_func = reloadable(exception_callback=mock_callback)(func)
+
+        with self.assertRaises(STOP_CONDITION_EXCEPTION):
+            reloadable_func()
+
+        for index, exception_cls in enumerate(exceptions[:-1]):
+            self.assertIsInstance(mock_callback.call_args_list[index][0][0],
+                                  exception_cls)
