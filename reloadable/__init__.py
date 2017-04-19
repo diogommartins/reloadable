01784d2938a68f37f8785839c0badd06d6153e24
Eduardo Matos <eduardo.matos@sieve.com.br>
2017-04-19 14:24:58 -0300

    fix typo

--- reloadable/__init__.py
+++ reloadable/__init__.py
@@ -6,5 +6,5 @@ def configure(**options):
     for option, value in options.items():
         config_name = option.upper()
         if not hasattr(config, config_name):
-            raise ValueError("Option '%s' doesnt exist for reloadable" % config_name)
+            raise ValueError("Option '%s' doesn't exist for reloadable" % config_name)
         setattr(config, config_name, value)
