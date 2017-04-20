8a38759e7204f8cd420f1106ad123c310d226735
diogommartins <magalhaesmartins@icloud.com>
2017-04-20 10:23:52 -0300

    Bumping package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from os import path
 from setuptools import setup, find_packages
 
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 
 here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst')) as file:
